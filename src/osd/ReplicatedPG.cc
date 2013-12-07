54e248f735b6bd47e448957262265c23c8e0479f
Samuel Just <sam.just@inktank.com>
2013-12-07 14:43:22 -0800

    ReplicatedPG: EOPNOTSUPP on ec_pool if there are omaps in the COPY_GET
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -4798,6 +4798,10 @@ void ReplicatedPG::process_copy_chunk(hobject_t oid, tid_t tid, int r)
 	     << " tid " << cop->objecter_tid << dendl;
     return;
   }
+
+  if (r >= 0 && pool.info.ec_pool() && cop->omap.size()) {
+    r = -EOPNOTSUPP;
+  }
   cop->objecter_tid = 0;
   ObjectContextRef& cobc = cop->obc;
 
